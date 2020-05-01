7d70121b30c489d5a02e72c47600518d72c18e3a
Matthew Black <mblackmblackmblack@gmail.com>
2020-05-01 14:42:18 -0400

    Fix linting

--- src/worker/loan/agent/new.js
+++ src/worker/loan/agent/new.js
@@ -1,62 +1,65 @@
-const axios = require('axios');
+const axios = require('axios')
 
-const { getEndpoint } = require('../../../utils/endpoints');
-const LoanMarket = require('../../../models/LoanMarket');
+const { getEndpoint } = require('../../../utils/endpoints')
+const LoanMarket = require('../../../models/LoanMarket')
 
-const { NETWORK, HEROKU_APP } = process.env;
+const web3 = require('../../../utils/web3')
+const { getInterval } = require('../../../utils/intervals')
 
-function defineNewAgentJobs(agenda) {
+const { NETWORK, HEROKU_APP } = process.env
+
+function defineNewAgentJobs (agenda) {
   agenda.define('notify-arbiter', async (job, done) => {
-    const loanMarkets = await LoanMarket.find().exec();
-    const loanMarket = loanMarkets[0];
+    const loanMarkets = await LoanMarket.find().exec()
+    const loanMarket = loanMarkets[0]
 
     const {
       collateralPublicKey,
-      principalAddress,
-    } = await loanMarket.getAgentAddresses();
+      principalAddress
+    } = await loanMarket.getAgentAddresses()
 
-    let url;
+    let url
     if (NETWORK === 'test') {
-      url = getEndpoint('LIQUIDATOR_ENDPOINT');
+      url = getEndpoint('LIQUIDATOR_ENDPOINT')
     } else if (HEROKU_APP !== undefined && HEROKU_APP !== 'undefined') {
-      url = `https://${HEROKU_APP}.herokuapp.com/api/loan`;
+      url = `https://${HEROKU_APP}.herokuapp.com/api/loan`
     } else {
-      done();
+      done()
     }
 
-    console.log('notify-arbiter');
+    console.log('notify-arbiter')
 
-    const ethSigner = process.env.METAMASK_ETH_ADDRESS;
+    const ethSigner = process.env.METAMASK_ETH_ADDRESS
 
-    const timestamp = Math.floor(new Date().getTime() / 1000);
-    const message = `Register new liquidator (${principalAddress} ${collateralPublicKey} ${ethSigner} ${url}) ${timestamp}`;
+    const timestamp = Math.floor(new Date().getTime() / 1000)
+    const message = `Register new liquidator (${principalAddress} ${collateralPublicKey} ${ethSigner} ${url}) ${timestamp}`
     const signature = await web3().eth.personal.sign(
       message,
       (await web3().currentProvider.getAddresses())[0]
-    );
+    )
 
     try {
-      console.log('posting...');
+      console.log('posting...')
       await axios.post(`${getEndpoint('ARBITER_ENDPOINT')}/liquidators/new`, {
         collateralPublicKey,
         principalAddress,
         ethSigner,
         url,
         signature,
-        timestamp,
-      });
-      done();
+        timestamp
+      })
+      done()
     } catch (e) {
-      console.log('`notify-arbiter` failed. Retrying...');
-      agenda.schedule(getInterval('ACTION_INTERVAL'), 'notify-arbiter');
-      console.log(e);
-      done(e);
+      console.log('`notify-arbiter` failed. Retrying...')
+      agenda.schedule(getInterval('ACTION_INTERVAL'), 'notify-arbiter')
+      console.log(e)
+      done(e)
     }
 
-    done();
-  });
+    done()
+  })
 }
 
 module.exports = {
-  defineNewAgentJobs,
-};
+  defineNewAgentJobs
+}
